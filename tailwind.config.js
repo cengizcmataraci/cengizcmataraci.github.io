c66a699cbb0d80c840f80c55e298332cb227b08f
cengizcmataraci <cengizcmataraci@gmail.com>
2021-12-31 00:56:10 +0300

    add typography plugin && purge unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  content: [
+  purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
