c4fde10dff48353074974642681fa0234f2b6b09
cengizcmataraci <cengizcmataraci@gmail.com>
2022-01-01 01:07:29 +0300

    implement dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { appWithTranslation } from "next-i18next";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default appWithTranslation(MyApp);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
