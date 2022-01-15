d32ce39c7bf734f57385204376b73482d88b3e46
cengizcmataraci <cengizcmataraci@gmail.com>
2022-01-15 21:44:30 +0300

    refactor tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@ module.exports = {
   darkMode: "class",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme("colors.gray.300"),
+            h1: {
+              color: theme("colors.gray.100"),
+            },
+            h2: {
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ["dark"],
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
