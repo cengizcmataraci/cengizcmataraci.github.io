ba33ce0af1d0daeac9d8937194dbf40b413c0c71
cengizcmataraci <cengizcmataraci@gmail.com>
2022-01-20 23:49:50 +0300

    implement framer-motion

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,8 +43,8 @@ export default function Layout({ children, home, about }) {
   // };
 
   const variants = {
-    hidden: { opacity: 0, x: -200, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
+    hidden: { opacity: 0, x: 0, y: 0 },
+    enter: { opacity: 1, x: 0, y: 100 },
     exit: { opacity: 0, x: 0, y: -100 },
   };
 
